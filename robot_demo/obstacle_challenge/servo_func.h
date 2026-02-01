ba2486d6e873fa9d47107062bf4ad74b4a727730
Tegh25 <teghveera@gmail.com>
2026-02-01 01:41:11 -0500

    Change servo pin to special pin 18

--- robot_demo/obstacle_challenge/servo_func.h
+++ robot_demo/obstacle_challenge/servo_func.h
@@ -5,7 +5,7 @@
 #include <Servo.h>
 
 // ============ SERVO CONFIGURATION ============
-#define SERVO_PIN A0        // PWM pin connected to servo signal
+#define SERVO_PIN 18        // PWM pin connected to servo signal
 #define SERVO_MIN_ANGLE 0   // Minimum servo angle (degrees)
 #define SERVO_MAX_ANGLE 180 // Maximum servo angle (degrees)
 #define SERVO_CENTER 90     // Center position (degrees)
